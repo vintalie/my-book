ebe4e190d2f7fe2ab36b21fb6e37254373ce0686
Vintalie <marioluizsan@gmail.com>
2024-09-11 22:00:41 -0300

    auth routes removed

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,10 +1,4 @@
-const bcrypt = require('bcryptjs');
-const User = require('../../models/User');
 const passport = require('passport')
-
-
-
-  
 
 module.exports = {
 
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const AuthController = require('../../controllers/auth')
 const UsersController = require('../../controllers/user')
 const router = express.Router()
 const {protectRoute} = require('../../auth.js')
 
 
-router.get('/user', protectRoute, UsersController.currentUserView )
-router.get('/user/:id', UsersController.specificUserView )
-router.put('/user/:id', UsersController.attUser )
-router.delete('/user/:id', UsersController.delUser )
+router.get('/user', protectRoute, UsersController.get )
+router.get('/user/:id', UsersController.get )
+router.put('/user/:id', UsersController.post )
+router.delete('/user/:id', UsersController.delete )
+
+router.get('/login', AuthController.loginView)
+router.get('/register', AuthController.registerView)
+router.post('/login', AuthController.authUser)
+router.post('/register', UsersController.post)
+
+router.get('/logout', AuthController.logoutUser)
 
 module.exports = router
